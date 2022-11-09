1420d4c18cc4e725056ceb293bdfd6139ce18099
Sajna <sanjat22@hotmail.com>
2022-11-09 21:47:06 +0100

    init commit

---
+++ src/main/java/com/code/saucedemo/pages/ProductsPages.java
@@ -0,0 +1,25 @@
+package com.code.saucedemo.pages;
+
+import org.openqa.selenium.WebElement;
+
+public class ProductsPages {
+
+    private WebDriver driver;
+
+    public Webdriver getDriver (){
+        return driver;
+
+    }
+
+
+    public void setDriver (WebDriver driver) {
+        this.driver = driver;
+    }
+    public LoginPage (WebDriver driver) {
+        this.driver = driver;
+
+    }
+    //mapiranje(nalazenje) elemenata
+    public WebElement. getInpUserName
+}
+
---
+++ src/main/java/com/code/saucedemo/pages/WebDriver.java
@@ -0,0 +1,3 @@
+package com.code.saucedemo.pages;
+
+public class WebDriver {
